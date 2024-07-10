029f479f82d6bb0aaa45c74387fc3b74b8339f9e
Pal Konyves <pkonyves@users.noreply.github.com>
2024-07-10 12:43:09 +0000

    fix region preferred list

--- Allfiles/labs/01/setup.ps1
+++ Allfiles/labs/01/setup.ps1
@@ -86,7 +86,7 @@ Write-Host "Finding an available region. This may take several minutes...";
 $delay = 0, 30, 60, 90, 120 | Get-Random
 Start-Sleep -Seconds $delay # random delay to stagger requests from multi-student classes
 #$preferred_list = "westeurope","australiaeast","centralus","southcentralus","eastus2","northeurope","southeastasia","uksouth","westus","westus2"
-$preferred_list = "westeurope"
+$preferred_list = "westeurope","northeurope"
 $locations = Get-AzLocation | Where-Object {
     $_.Providers -contains "Microsoft.Synapse" -and
     $_.Providers -contains "Microsoft.Sql" -and
